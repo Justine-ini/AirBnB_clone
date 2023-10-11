b5478cd1e1dde17de02683122ce986ad51a8b719
Ololade Victor <oluwabori2014@gmail.com>
2023-10-11 00:51:09 -0700

    Update

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A base model representaion """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """ A class representation of the base model """
+
+    def __init__(self, *args, **kwargs):
+        """ Instatialisation of the init method"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Returns string info about this class """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ Saves info about this class """
+        updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        return obj_dict
