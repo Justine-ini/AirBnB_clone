7c38ff731400ee9a7be17334bde4b812a1520186
Ololade Victor <oluwabori2014@gmail.com>
2023-10-11 08:01:36 -0700

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,18 @@ class BaseModel:
     """ A class representation of the base model """
 
     def __init__(self, *args, **kwargs):
-        """ Instatialisation of the init method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """ Instatialisation of the init method """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Returns string info about this class """
