2f080aa93ca65987f6e5ffa782f35d9f4ba0647a
Justine-ini <inijustine4040@gmail.com>
2023-10-13 02:48:26 -0700

    AirBnB clone

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ A base model representaion """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -13,21 +14,27 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                        setattr(
+                            self,
+                            key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
+                        )
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ Returns string info about this class """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """ Saves info about this class """
         updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance"""
