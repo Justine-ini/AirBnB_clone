2520fcd4e361b7bacd27c6636b07403947eca46a
Justine-ini <inijustine4040@gmail.com>
2023-10-14 15:31:26 -0700

    Updaate *args, **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,30 @@ from datetime import datetime
 class BaseModel:
     """A base class for all other classes in the models directory."""
 
-    def __init__(self):
-        """Initialize a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel.
+
+        Attributes:
+        - id: str - a unique identifier (UUID) converted to a string.
+        - created_at: set to the current datetime when the instance is created
+        - updated_at: set to current datetime when the instance is created
+        """
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ["created_at" "updated_at"]:
+                    value = datetime.fromisoformat(value)
+
+                # Set the attribute on the object.
+                else:
+                    setattr(self, key, value)
+
+        # Otherwise, create new `id` and `created_at` attributes.
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
