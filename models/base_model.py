df0ac9bf2a0ad3d7f81350f521700a4b199fe535
Justine-ini <inijustine4040@gmail.com>
2023-10-14 11:37:11 -0700

    Bugfix

--- models/base_model.py
+++ models/base_model.py
@@ -2,39 +2,24 @@
 """ A base model representaion """
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-    """ A class representation of the base model """
+    """A base class for all other classes in the models directory."""
 
-    def __init__(self, *args, **kwargs):
-        """ Instatialisation of the init method """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(
-                            self,
-                            key,
-                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
-                        )
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+    def __init__(self):
+        """Initialize a new BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Returns string info about this class """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Return a string representation of the BaseModel instance."""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        """ Saves info about this class """
-        updated_at = datetime.now()
-        storage.save()
+        """ Returns string info about this class """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance"""
