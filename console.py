8527000151192f39707ddcff9242a21c94b01acf
Justine-ini <inijustine4040@gmail.com>
2023-10-14 09:30:54 -0700

    pycodestyle

--- console.py
+++ console.py
@@ -8,7 +8,7 @@ class HBNBCommand(cmd.Cmd):
 
     """Class for the command interpreter."""
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
