3c4cc51bb555de7f554de733adf8dafbe76dfcad
Justine-ini <inijustine4040@gmail.com>
2023-10-14 09:11:00 -0700

    made files executable

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on Ctrl+D (EOF)"""
+        print()
+        return True
+
+    def do_help(self, arg):
+        """Show help information"""
+        super().do_help(arg)
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
