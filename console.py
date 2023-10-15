439ee0352453ca5c27d6c4d7a9843ace214c6d70
Justine-ini <inijustine4040@gmail.com>
2023-10-14 19:39:08 -0700

    AirBnB clone - The console

---
+++ console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This is the console for AirBnB"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    """Class for the command interpreter."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on Ctrl+D (EOF)"""
+        print()
+        return True
+
+    def do_help(self, arg):
+        """Show help information"""
+        super().do_help(arg)
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
